b832f6d08b7596dc88a228136ef7f801c3bf10e6
Eric Helstad <102440393+cypherbean-io@users.noreply.github.com>
2025-03-30 10:09:25 -0500

    Optimize using rubocop

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -1,17 +1,19 @@
+# frozen_string_literal: true
+
 # Method to break down the string into an array of characters and perform shift if the character is a letter
 def caesar_cipher(string, shift)
-  string.split('').map do |char|
-    if char.match(/[a-zA-Z]/)
+  string.chars.map do |char|
+    if /[a-zA-Z]/.match?(char)
       shift_char(char, shift)
     else
       char
     end
-  end.join('')
+  end.join
 end
 
 # Method to shift the character by the shift value
 def shift_char(char, shift)
-  shift = shift % 26
+  shift %= 26
   char = char.ord
   if char.between?(65, 90)
     char = ((char + shift - 65) % 26) + 65
