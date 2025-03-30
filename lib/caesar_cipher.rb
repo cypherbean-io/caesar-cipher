696db474c83424ad6e4d2500a1e5001c08627aa0
Eric Helstad <102440393+cypherbean-io@users.noreply.github.com>
2025-03-30 10:39:34 -0500

    Refactor caesar_cipher.rb to be a class

--- lib/caesar_cipher.rb
+++ lib/caesar_cipher.rb
@@ -1,26 +1,29 @@
 # frozen_string_literal: true
 
-# Method to break down the string into an array of characters and perform shift if the character is a letter
-def caesar_cipher(string, shift)
-  string.chars.map do |char|
-    if /[a-zA-Z]/.match?(char)
-      shift_char(char, shift)
-    else
-      char
-    end
-  end.join
-end
-
-# Method to shift the character by the shift value
-def shift_char(char, shift)
-  shift %= 26
-  char = char.ord
-  if char.between?(65, 90)
-    char = ((char + shift - 65) % 26) + 65
-  elsif char.between?(97, 122)
-    char = ((char + shift - 97) % 26) + 97
+# Description/Explanation:
+# This code implements a Caesar cipher, which is a type of substitution cipher where each letter in the plaintext is
+# 'shifted' a certain number of places down or up the alphabet.
+class CaesarCipher
+  # Method to break down the string into an array of characters and perform shift if the character is a letter
+  def encrypt(string, shift)
+    string.chars.map do |char|
+      if /[a-zA-Z]/.match?(char)
+        shift_char(char, shift)
+      else
+        char
+      end
+    end.join
   end
-  char.chr
-end
 
-puts caesar_cipher('Sweet Home Alabama', 20)
+  # Method to shift the character by the shift value
+  def shift_char(char, shift)
+    shift %= 26
+    char = char.ord
+    if char.between?(65, 90)
+      char = ((char + shift - 65) % 26) + 65
+    elsif char.between?(97, 122)
+      char = ((char + shift - 97) % 26) + 97
+    end
+    char.chr
+  end
+end
